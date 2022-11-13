347144f8366410794218b724cab0816602d7d580
Darji Parth <darjiparth@darjis-air.mynetworksettings.com>
2022-11-12 23:04:10 -0500

    add all the required patient routes with require request type

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -1,4 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
+router
+  .route('/')
+  .post(async (req, res) => {
+
+  })
+  
+  router
+  .route('/login')
+  .post(async (req, res) => {
+
+  })
+
+  router
+  .route('/:patientId')
+  .get(async (req, res) => {
+
+  })
+  .patch(async (req, res) => {
+
+  })
+
+  router
+  .route('/:patientId/appointment')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:patientId/chat/:doctorId')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:patientId/prescription')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:patientId/medicalHistory')
+  .get(async (req, res) => {
+
+  })
+  .post(async (req, res) => {
+
+  })
+
+  router
+  .route('/:patientId/medicalHistory/:medicalHistoryId')
+  .patch(async (req, res) => {
+
+  })
+
+  router
+  .route('/:patientId/testReport')
+  .get(async (req, res) => {
+
+  })
+  .post(async (req, res) => {
+
+  })
+
+  router
+  .route('/:patientId/testReport/:testReportId')
+  .patch(async (req, res) => {
+
+  })
+  
 module.exports = router;
