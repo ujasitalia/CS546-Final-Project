f9187c2fff649d8f6a1a92cda467962a313309e8
Darji Parth <darjiparth@darjis-air.mynetworksettings.com>
2022-11-12 23:16:06 -0500

    add all the required healthy habits routes with require request type

--- backend/routes/healthyHabit.js
+++ backend/routes/healthyHabit.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router
+  .route('/')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:healthyHabitId')
+  .get(async (req, res) => {
+
+  })
+  
 module.exports = router;
