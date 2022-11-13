76a60673d4b82c0f852004a5d6cfae58017ff96b
Darji Parth <darjiparth@darjis-air.mynetworksettings.com>
2022-11-12 22:34:22 -0500

    created require route files with required structure and defined basic routes in index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,17 @@
+const doctorRoutes = require('./doctor');
+const patientRoutes = require('./patient');
+const reviewRoutes = require('./review');
+const appointmentRoutes = require('./appointment');
+const chatRoutes = require('./chat');
+const healthyHabitRoutes = require('./healthyHabit');
 
 const constructorMethod = (app) => {
-
+  app.use('/doctor', doctorRoutes);
+  app.use('/patient', patientRoutes);
+  app.use('/review', reviewRoutes);
+  app.use('/appointment', appointmentRoutes);
+  app.use('/chat', chatRoutes);
+  app.use('/healthyHabit', healthyHabitRoutes);
   app.use('*', (req, res) => {
     res.status(404).json({error: 'Not found'});
   });
