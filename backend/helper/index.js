396588583f07c95fd5b41ebbc581b0c29ba8b3a1
Darji Parth <darjiparth@darjis-air.mynetworksettings.com>
2022-11-12 23:53:10 -0500

    created helper folder with all require helper files

---
+++ backend/helper/index.js
@@ -0,0 +1,17 @@
+const commonHelpers = require('./common');
+const doctorHelpers = require('./doctor');
+const patientHelpers = require('./patient');
+const reviewHelpers = require('./review');
+const appointmentHelpers = require('./appointment');
+const chatHelpers = require('./chat');
+const healthyHabitHelpers = require('./healthyHabit');
+
+module.exports = {
+common : commonHelpers,
+doctor : doctorHelpers,
+patient : patientHelpers, 
+review : reviewHelpers,
+appointment : appointmentHelpers,
+chat : chatHelpers,
+healthyHabit : healthyHabitHelpers
+}
