09e633048aa0f5977f35047c92ba94fc5011fbbe
Darji Parth <darjiparth@darjis-air.mynetworksettings.com>
2022-11-12 23:10:42 -0500

    add all the required appointment routes with require request type

--- backend/routes/appointment.js
+++ backend/routes/appointment.js
@@ -1,4 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+router
+  .route('/')
+  .post(async (req, res) => {
+
+  })
+
+  router
+  .route('/:appointmentId')
+  .get(async (req, res) => {
+
+  })
+  .patch(async (req, res) => {
+
+  })
+  .delete(async (req, res) => {
+
+  })
+
 module.exports = router;
