d6bb515b28e67477d3bcabdbde2f7a0660b0880f
Darji Parth <darjiparth@darjis-air.mynetworksettings.com>
2022-11-12 22:51:12 -0500

    add all the require doctor routes with require request type

--- backend/routes/doctor.js
+++ backend/routes/doctor.js
@@ -1,4 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
+router
+  .route('/')
+  .get(async (req, res) => {
+
+  })
+  .post(async (req, res) => {
+
+  })
+  
+  router
+  .route('/login')
+  .post(async (req, res) => {
+
+  })
+
+  router
+  .route('/:doctorId')
+  .get(async (req, res) => {
+
+  })
+  .patch(async (req, res) => {
+
+  })
+
+  router
+  .route('/:doctorId/appointment')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:doctorId/slot')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:doctorId/chat/:patientId')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:doctorId/review')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:doctorId/patient/:patientId')
+  .get(async (req, res) => {
+
+  })
+
+  router
+  .route('/:doctorId/patient/:patientId/prescription')
+  .get(async (req, res) => {
+
+  })
+  .post(async (req, res) => {
+
+  })
+
+  router
+  .route('/:doctorId/patient/:patientId/prescription/:prescriptionId')
+  .patch(async (req, res) => {
+
+  })
+  
 module.exports = router;
