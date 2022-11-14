b5baae465705450ba83d38a7df19d756f4f48cc9
Megh Shah <Megh@meghs-air.mynetworksettings.com>
2022-11-14 15:15:52 -0500

    created routes for getPatientById and createPatient

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const patientData = require('../data/patient')
+const commonHelper = require('../helper/common');
+const patientHelper = require('../helper/patient');
 
 router
   .route('/')
   .post(async (req, res) => {
+    try{
+      const bodyData = req.body;
+      let email=commonHelper.isValidEmail(bodyData.email);
+      let age = patientHelper.isValidAge(bodyData.age);
+      let profilePicture=commonHelper.isValidFilePath(bodyData.profilePicture);
+      let name=commonHelper.isValidName(bodyData.name);
+      let city=commonHelper.isValidCity(bodyData.city);
+      let state=commonHelper.isValidState(bodyData.state);
+      let zip=commonHelper.isValidZip(bodyData.zip);
+      let password=commonHelper.isValidPassword(bodyData.password);
+      
+      let newPatient = await patientData.createPatient(email,age,profilePicture,name,city,state,zip,password);
+      res.json(newPatient);
 
+    }catch(e){
+      if(e.status)
+      {
+        res.status(e.status).json(e.error);
+      }
+      else
+        res.status(404).json(e);
+    }
   })
   
   router
@@ -16,7 +40,18 @@ router
   router
   .route('/:patientId')
   .get(async (req, res) => {
-
+    try{
+      req.params.patientId = commonHelper.isValidId(req.params.patientId);
+      const patient = await patientData.getPatientById(req.params.patientId);
+      res.status(200).json(patient);
+    }catch(e){
+      if(e.status)
+      {
+        res.status(e.status).json(e.error);
+      }
+      else
+        res.status(404).json(e);
+    }
   })
   .patch(async (req, res) => {
 
