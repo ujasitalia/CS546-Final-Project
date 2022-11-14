fbb91ea5e5bae80de591fc11784feaa4d89efbaa
Megh Shah <Megh@meghs-air.mynetworksettings.com>
2022-11-14 02:33:37 -0500

    Helper function added for checking age of patient

--- backend/helper/patient.js
+++ backend/helper/patient.js
@@ -1,5 +1,7 @@
-
+const isValidAge = (age) => {
+    if(!age || typeof age!='number' || age<1 || age>120 || age%1) throw {status:400, error:'Invalid age'};
+}
 
 module.exports = {
-    
+    isValidAge
 };
