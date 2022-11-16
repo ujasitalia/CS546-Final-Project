4092623653b9a382510085538e177cb3a70e2fdd
Dhru Chapadia <cdhru2000@gmail.com>
2022-11-16 16:03:44 -0500

    Backend review functions

--- backend/data/review.js
+++ backend/data/review.js
@@ -1,5 +1,76 @@
+const helper = require('../helper');
+const mongoCollections = require('../config/mongoCollections');
+const reviewCol = mongoCollections.review;
+const {ObjectId} = require('mongodb');
+const patient = require('./patient');
+const doctor = require('./doctor');
+const createReview = async(
+doctorID,
+patientId,
+reviewText,
+rating
+) => {
+    doctorData = await doctor.getDoctorById(doctorID);
+    patientData = await patient.getDoctorById(patientId);
+    if(!reviewText){
 
+    }
+    rating = helper.review.checkRating(rating);
 
+    const reviewCollection = await reviewCol();
+    if(!reviewText){
+        const newReview = {
+            doctorID,
+            patientId,
+            rating
+          };
+    }else{
+    const newReview = {
+        doctorID,
+        patientId,
+        reviewText,
+        rating
+      };
+    }
+  
+    const insertInfo = await reviewCollection.insertOne(newReview);
+  
+    if (!insertInfo.acknowledged || !insertInfo.insertedId)
+        throw {status: '500', error : 'Could not add review'};
+  
+    const newId = insertInfo.insertedId.toString();
+    const review = await getreviewById(newId);
+  
+    return review;
+}
+const getreviewById = async(reviewId) =>{
+    reviewId = helper.common.isValidId(reviewId);
+
+    const reviewCollection = await reviewCol();
+    const review = await reviewCollection.findOne({_id: ObjectId(reviewId)});
+
+    if (review === null) 
+    {
+        throw {status: '404', error : 'No review with that id'};
+    }
+
+    review._id = review._id.toString();
+
+    return review;
+}
+const getAllreview = async (doctorID) => {
+    reviewId = helper.common.isValidId(doctorID);
+
+    const reviewCollection = await reviewCol();
+    const reviewsArray = await reviewCollection.find({'review.doctorID': doctorID})
+    .toArray();find().toArray();
+  
+    if (!reviewsArray) throw {status: '500', error : 'Could not get all doctors'};
+  
+    return reviewsArray;
+  };
 module.exports = {
-    
+    createReview,
+    getreviewById,
+    getAllreview
 };
--- backend/helper/review.js
+++ backend/helper/review.js
@@ -1,5 +1,14 @@
-
+const checkRating = (rating) => {
+    rating = rating.trim();
+    if(typeof rating !== 'string'){
+      throw 'not a string';
+    }
+    if(rating !== 'G' && rating !== 'PG' && rating !== 'PG-13' && rating !== 'R' && rating !== 'NC-17'){
+        throw 'not proper rating';
+    }
+    return rating;
+  };
 
 module.exports = {
-    
+    checkRating
 };
