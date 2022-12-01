4a3978ccbb722b04642a5bb26fbb62d85373f36a
Megh Shah <50547274+megh7199@users.noreply.github.com>
2022-12-01 15:22:48 -0500

    Update patient.js
    refactoring isValidAge

--- backend/helper/patient.js
+++ backend/helper/patient.js
@@ -1,6 +1,7 @@
 const commonHelper = require('./common');
 
 const isValidAge = (age) => {
+    age = parseInt(age)
     if(!age || typeof age!='number' || age<1 || age>120 || age%1) throw {status:400, error:'Invalid age'};
     return age;
 }
