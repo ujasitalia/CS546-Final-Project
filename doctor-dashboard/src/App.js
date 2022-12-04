7c56df37a2eabc9a5372542f6a6214e47d6070d3
Darji Parth <darjiparth@darjis-air.mynetworksettings.com>
2022-12-04 02:27:35 -0500

    created doctor profile page, logout functionality and made all page independent of each other

--- doctor-dashboard/src/App.js
+++ doctor-dashboard/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import {pages} from "./pages";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
           <Route path="/login" element={<pages.Login/>} />
           <Route path="/" element={<Navigate  to="/login"/>}/>
           <Route path="/dashboard" element={<pages.Dashboard/>}/>
+          <Route path="/profile" element={<pages.Profile/>}/>
           {/* <Route path="/create-account" element={<CreateAccount/>} /> */}
         </Routes>
       </Router>
