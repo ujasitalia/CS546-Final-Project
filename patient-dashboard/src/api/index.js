3c4331992e9bea192a4b870e3e6a1d9850bb9df8
Darji Parth <darjiparth@pds-macbook-air.mynetworksettings.com>
2022-12-13 21:41:57 -0500

    created patient chatBox

--- patient-dashboard/src/api/index.js
+++ patient-dashboard/src/api/index.js
@@ -2,13 +2,13 @@ import * as login from "./login"
 import * as doctor from "./doctor"
 import * as appointment from './appointment'
 import * as patient from "./patient"
+import * as chat from "./chat";
 
 const api = {
     login,
-    search, 
-    filter,
     doctor,
     appointment,
     patient,
+    chat
   }
 export { api }
