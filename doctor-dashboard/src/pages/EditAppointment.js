c2432e0e6f33fdc146ab812fef153175fa347b6b
Parth Darji <55014209+pd998@users.noreply.github.com>
2022-12-14 10:05:52 -0500

    Update EditAppointment.js

--- doctor-dashboard/src/pages/EditAppointment.js
+++ doctor-dashboard/src/pages/EditAppointment.js
@@ -6,7 +6,6 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Chat from '../chat'
 
 const EditAppointment = () => {
   const { appointmentId } = useParams();
